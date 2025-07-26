d723383df57be7ff5f8fa5f3626fd273cc3a46f7
evannsm <evannsmcuadrado@gmail.com>
2025-07-26 13:00:00 -0400

    changed readme and added all files

---
+++ immrax/__init__.py
@@ -0,0 +1,48 @@
+from .inclusion import *
+
+from . import system as system
+from .system import (
+    System,
+    ReversedSystem,
+    LinearTransformedSystem,
+    LiftedSystem,
+    OpenLoopSystem,
+)
+
+from . import control as control
+from .control import (
+    Control,
+    ControlledSystem,
+    LinearControl,
+    # FOHControlledSystem,
+)
+
+try:
+    import jax_verify
+    from . import neural as neural
+    from .neural import (
+        NeuralNetwork,
+        CROWNResult,
+        FastlinResult,
+        crown,
+        fastlin,
+        NNCSystem,
+        NNCEmbeddingSystem,
+    )
+except ImportError:
+    pass
+
+from . import embedding as embedding
+from .embedding import (
+    EmbeddingSystem,
+    InclusionEmbedding,
+    TransformEmbedding,
+    ifemb,
+    natemb,
+    jacemb,
+    mjacemb,
+    embed,
+)
+
+# from . import refinement as refinement
+from . import utils as utils
